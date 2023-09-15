cebf17709b9988b3f23d7d4c5647a379215bd815
Leticia Almeida <leticiapaulinoferreira@gmail.com>
2023-09-15 18:32:18 -0300

    lista

--- lista-list/README.md
+++ lista-list/README.md
@@ -1,2 +1,43 @@
-# Lista
-list 
+# Lista encadeada
+linked list 
+
+Estrutura de dados linear que coresponde a diversos nós conectados, em que cada nó guarda o o dado e o endereço do próximo nó. Para o primeiro nó da-se o nome `head` e o último aponta para `null`. Existem diversos tipos de listas encadeadas.
+
+![exemplo-linked-list](https://cdn.programiz.com/sites/tutorial2program/files/linked-list-concept.png)
+
+### Programação
+Estrutura do nó
+```
+struct node
+{
+  int data;
+  struct node *next;
+};
+```
+
+Criando a lista
+```
+/* Initialize nodes */
+struct node *head;
+struct node *one = NULL;
+struct node *two = NULL;
+struct node *three = NULL;
+
+/* Allocate memory */
+one = malloc(sizeof(struct node));
+two = malloc(sizeof(struct node));
+three = malloc(sizeof(struct node));
+
+/* Assign data values */
+one->data = 1;
+two->data = 2;
+three->data=3;
+
+/* Connect nodes */
+one->next = two;
+two->next = three;
+three->next = NULL;
+
+/* Save address of first node in head */
+head = one;
+```
--- lista-list/lista.c
+++ lista-list/lista.c
@@ -1,121 +1,43 @@
-/* ================================================================== *
- *  PROJETO (PARTE 1)
- *  Estrutura de dados
- *  Bruna Scarpelli - 800435
- *  Letícia Almeida P. de A. Ferreira - 800480
- * ================================================================== */ 
-
 #include <stdio.h>
 #include <stdlib.h>
-#include "lista.h" 
 
-void inicializarLista(TLista *l)
-{
-  struct NO *n = (struct NO*)malloc(sizeof(struct NO));
-  n->prox = n->anterior = l->sentinela = n;
-  l->quantidade = 0;
-}
+// Creating a node
+struct node {
+  int value;
+  struct node *next;
+};
 
-void destruir(TLista *l)
-{
-  while(!vaziaLista(l)){
-    removerFimLista(l);
-  }
-  free(l->sentinela);
-}
-
-int vaziaLista(TLista *l)
-{
-  return l->sentinela->prox == l->sentinela; 
-}
-
-void inserirInicioLista(TLista *l, int dado)
-{
-  struct NO *n = (struct NO*)malloc(sizeof(struct NO));
-  n->dado = dado;
-  n->prox = l->sentinela->prox;
-  n->anterior = l->sentinela;
-  l->sentinela->prox = n;
-  n->prox->anterior = n;
-  l->quantidade++;
-}
-
-void inserirFimLista(TLista *l, int dado)
-{
-  struct NO *n = (struct NO*)malloc(sizeof(struct NO));
-  n->dado = dado;
-  n->anterior = l->sentinela->anterior;
-  n->prox = l->sentinela;
-  l->sentinela->anterior = n;
-  n->anterior->prox = n;
-  l->quantidade++;
-}
-
-void removerFimLista(TLista *l)  
-{
-  struct NO *n = l->sentinela -> anterior;
-  
-  if( n != l->sentinela )
-  { // lista vazia
-    n->prox->anterior = n->anterior;
-    n->anterior->prox = n->prox;
-    free(n);
-    l -> quantidade--;
+// print the linked list value
+void printLinkedlist(struct node *p) {
+  while (p != NULL) {
+    printf("%d ", p->value);
+    p = p->next;
   }
 }
 
-void removerInicioLista(TLista *l)  
-{
-  struct NO *n = l->sentinela -> prox;
-  
-  if( n != l->sentinela )
-  { // lista vazia
-    n->anterior->prox = n->prox;
-    n->prox->anterior = n->anterior;
-    free(n);
-    l -> quantidade--;
-  }
-}
+int main() {
+  // Initialize nodes
+  struct node *head;
+  struct node *one = NULL;
+  struct node *two = NULL;
+  struct node *three = NULL;
 
-int length(TLista *l) {
-  int t;
-  struct NO *n;
+  // Allocate memory
+  one = malloc(sizeof(struct node));
+  two = malloc(sizeof(struct node));
+  three = malloc(sizeof(struct node));
 
-  if (vaziaLista(l)) {
-    return 0;
-  } 
+  // Assign value values
+  one->value = 1;
+  two->value = 2;
+  three->value = 3;
 
-  t = 1;
-  n = l->sentinela->prox;
+  // Connect nodes
+  one->next = two;
+  two->next = three;
+  three->next = NULL;
 
-  while(n != l->sentinela) {
-    n = n->prox;
-    t++;
-  }
-return t-1;
-}
-
-void imprimirLista(TLista *l, int asc) {
-  struct NO *n;
-
-   if (vaziaLista(l)) {
-    printf("Lista vazia\n");
-  } 
-  else{
-    printf("Lista possui %d elemento(s): \n", length(l));
-  
-    n = l-> sentinela->prox;
-
-  do{
-    printf("-> %d\n", n->dado);
-
-    if (asc) {
-      n = n->prox; 
-    } else {
-      n = n->anterior;
-      }
-    
-    } while(n != l->sentinela);
-  }
-  printf("\n");
+  // printing node-value
+  head = one;
+  printLinkedlist(head);
 }
